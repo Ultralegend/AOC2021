f6703de2feb08359646b70ce62d638a895ce9657
Ultralegend <johnphilipcarreon@gmail.com>
2021-12-01 13:54:16 +0800

    first day answers

--- Day01/aoc.py
+++ Day01/aoc.py
@@ -1,10 +1,20 @@
 import os
-
 os.system("cls")
 
-fp = open("input.txt", "r")
+fp = open("Data/input.txt", "r")
+
+prev = int(fp.readline().strip())
+counter = 0
 
 for i in fp:
-    print(i)
+    current = int(i.strip())
+    
+    if (current > prev):
+        counter += 1
+
+    prev = current
 
 fp.close()
+
+print(counter)
+
