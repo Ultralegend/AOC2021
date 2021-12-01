f67f5c8f93114bd958e5022368cd48b7ff42a9db
Ultralegend <johnphilipcarreon@gmail.com>
2021-12-02 07:28:10 +0800

    consolidated answers into functions

--- Day01/aoc.py
+++ Day01/aoc.py
@@ -1,19 +1,26 @@
 import os
 os.system("cls")
 
+def countIncrease(array):
+    increase = 0
+    for i in range(0, len(array) - 1):
+        if (array[i] < array[i+1]):
+            increase += 1
+    return increase
+
+def countIncreaseBy3(array):
+    increase = 0
+    for i in range(0, len(array) - 3):
+        sumA = inputNum[i] + inputNum[i + 1] + inputNum[i + 2]
+        sumB = inputNum[i + 1] + inputNum[i + 2] + inputNum [i + 3]
+        if (sumA < sumB):
+            increase += 1
+    return increase
+
+
 fp = open("Data/input.txt", "r")
-
-prev = int(fp.readline().strip())
-counter = 0
-
-for i in fp:
-    current = int(i.strip())
-    
-    if (current > prev):
-        counter += 1
-
-    prev = current
-
+inputNum = [int(i) for i in fp]
 fp.close()
 
-print(counter)
+print(countIncrease(inputNum))
+print(countIncreaseBy3(inputNum))
