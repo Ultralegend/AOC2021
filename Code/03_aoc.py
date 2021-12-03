1069367bddf883da42ee36bf76c89807fa1e5927
Ultralegend <johnphilipcarreon@gmail.com>
2021-12-03 13:43:40 +0800

    add day 3 solution and input

--- Code/03_aoc.py
+++ Code/03_aoc.py
@@ -1,10 +1,92 @@
 import os
 os.system("cls")
 
+def getPowerRating(array):
+    gamma = ""
+    epsilon = ""
+    record = {}
+
+    for i in range(0, len(array[0])):
+        record[0] = 0
+        record[1] = 0
+
+        for j in array:
+            if (j[i] == "1"):
+                record[1] += 1
+            else:
+                record[0] += 1
+
+        if (record[1] > record[0]):
+            gamma += "1"
+            epsilon += "0"
+        else:
+            gamma += "0"
+            epsilon += "1"
+    
+    return int(gamma, 2) * int(epsilon, 2)
+
+def getO2Rating(array):
+    record = {}
+
+    for i in range(0, len(array[0])):
+        newArray = []
+        record[0] = 0
+        record[1] = 0
+
+        for j in array:
+            if (j[i] == "1"):
+                record[1] += 1
+            else:
+                record[0] += 1
+
+        if (record[0] > record[1]):
+            for j in array:
+                if (j[i] == "0"):
+                    newArray.append(j)
+        else:
+            for j in array:
+                if (j[i] == "1"):
+                    newArray.append(j)
+
+        
+        array = newArray
+
+        if (len(array) == 1):
+            return int(array[0], 2)
+
+def getCO2Rating(array):
+    record = {}
+
+    for i in range(0, len(array[0])):
+        newArray = []
+        record[0] = 0
+        record[1] = 0
+
+        for j in array:
+            if (j[i] == "1"):
+                record[1] += 1
+            else:
+                record[0] += 1
+
+        if (record[1] < record[0]):
+            for j in array:
+                if (j[i] == "1"):
+                    newArray.append(j)
+        else:
+            for j in array:
+                if (j[i] == "0"):
+                    newArray.append(j)
+
+        
+        array = newArray
+
+        if (len(array) == 1):
+            return int(array[0], 2)
+            
+
 fp = open("../Input/03_input.txt", "r")
-inputNum = [int(i) for i in fp]
-#inputNum = []
-#for i in fp: inputNum.append(int(i))
+inputNum = [i.strip() for i in fp]
 fp.close()
 
-print(inputNum)
+print(getPowerRating(inputNum))
+print(getO2Rating(inputNum) * getCO2Rating(inputNum))
