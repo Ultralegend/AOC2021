4f82dc786a366e3e185e2325c98ffbd2ff83633d
Ultralegend <johnphilipcarreon@gmail.com>
2021-12-09 15:08:02 +0800

    add day9 solution and day10 template

--- Code/09_aoc.py
+++ Code/09_aoc.py
@@ -1,15 +1,77 @@
 import os
 os.system("cls")
-print("\n\nNEW INPUT\n")
 
 def arrayToInt(array): return [int(i) for i in array]
 
-def removeArray(array, remove): return [i for i in array if (i != remove)]
+def checkSides(data, num, c):
+    if (c[0] - 1 != -1 and num >= data[c[0] - 1][c[1]]):
+        return False
+    
+    if (c[0] + 1 != len(data) and num >= data[c[0] + 1][c[1]]):
+        return False
+
+    if (c[1] - 1 != -1 and num >= data[c[0]][c[1] - 1]):
+        return False
+
+    if (c[1] + 1 != len(data[0]) and num >= data[c[0]][c[1] + 1]):
+        return False
+
+    return True
+
+def formBasin(data, c):
+    frontier = c
+    visited = []
+    basin = []
+
+    while (len(frontier) != 0):
+        x, y = frontier.pop(0)
+        num = data[x][y]
+        
+        if ([x, y] not in visited): 
+            visited.append([x, y])
+            basin.append(num)
+
+        if (x - 1 != -1 and data[x - 1][y] != 9 and num < data[x - 1][y]):
+            if ([x - 1, y] not in visited): 
+                frontier.append([x - 1, y])
+    
+        if (x + 1 != len(data) and data[x + 1][y] != 9 and num < data[x + 1][y]):
+            if ([x + 1, y] not in visited): 
+                frontier.append([x + 1, y])
+
+        if (y - 1 != -1 and data[x][y - 1] != 9 and num < data[x][y - 1] ):
+            if ([x, y - 1] not in visited): 
+                frontier.append([x, y - 1])
+
+        if (y + 1 != len(data[0]) and data[x][y + 1] != 9 and num < data[x][y + 1]):
+            if ([x, y + 1] not in visited): 
+                frontier.append([x, y + 1])
+
+    return len(basin)
+
+def getRiskLevel(data):
+    count = 0
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if checkSides(data, data[i][j],[i,j]):
+                count += data[i][j] + 1
+    
+    return count
+
+def getMulBasins(data):
+    basins = []
+
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if checkSides(data, data[i][j],[i,j]):
+                basins.append(formBasin(data, [[i,j]]))
+
+    basins.sort(reverse = True)
+    return basins[0] * basins[1] * basins[2]
 
 fp = open("../Input/09_input.txt", "r").read().split("\n")
 
-inputNum = []
-for i in fp: 
-    inputNum.append(i)
+inputNum = [arrayToInt(list(i)) for i in fp]
 
-print(inputNum)
+print(getRiskLevel(inputNum))
+print(getMulBasins(inputNum))
---
+++ Code/10_aoc.py
@@ -0,0 +1,15 @@
+import os
+os.system("cls")
+print("\n\nNEW INPUT\n")
+
+def arrayToInt(array): return [int(i) for i in array]
+
+def removeArray(array, remove): return [i for i in array if (i != remove)]
+
+fp = open("../Input/10_input.txt", "r").read().split("\n")
+
+inputNum = []
+for i in fp: 
+    inputNum.append(i)
+
+print(inputNum)
