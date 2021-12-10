97fa496884c797d2e20ad93cb3b398d2920902d6
Ultralegend <johnphilipcarreon@gmail.com>
2021-12-10 14:05:27 +0800

    add day10 solution and day11 template

--- Code/10_aoc.py
+++ Code/10_aoc.py
@@ -1,15 +1,62 @@
 import os
 os.system("cls")
-print("\n\nNEW INPUT\n")
 
-def arrayToInt(array): return [int(i) for i in array]
+def errorScore(data):
+    closePair = {"(" : ")", "[" : "]", "{" : "}", "<" : ">"}
+    score = 0
 
-def removeArray(array, remove): return [i for i in array if (i != remove)]
+    for i in data:
+        stack = []
+        
+        for j in i:
+            if (j in "([{<"):
+                stack.append(j)
+            else:
+                if (closePair[stack.pop()] == j): continue
+
+                if (j == ")"): score += 3
+                elif (j == "]"): score += 57
+                elif (j == "}"): score += 1197
+                else: score += 25137
+
+                break
+    
+    return score
+
+def incompleteScore(data):
+    closePair = {"(" : ")", "[" : "]", "{" : "}", "<" : ">"}
+    scores = []
+
+    for i in data:
+        error = False
+        stack = []
+        score = 0
+        
+        for j in i:
+            if (j in "([{<"):
+                stack.append(j)
+            else:
+                if (closePair[stack.pop()] == j): continue
+
+                error = True
+                break
+
+        if (not error):
+            for i in reversed(stack):
+                score *= 5
+
+                if (i == "("): score += 1
+                elif (i == "["): score += 2
+                elif (i == "{"): score += 3
+                else: score += 4
+        
+            scores.append(score)
+    
+    return sorted(scores)[len(scores) // 2]
 
 fp = open("../Input/10_input.txt", "r").read().split("\n")
 
-inputNum = []
-for i in fp: 
-    inputNum.append(i)
+inputNum = [i for i in fp]
 
-print(inputNum)
+print(errorScore(inputNum))
+print(incompleteScore(inputNum))
