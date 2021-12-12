321ed01dc1deac80d1ed5f423fa28534119ddc31
Ultralegend <johnphilipcarreon@gmail.com>
2021-12-12 16:09:43 +0800

    add day12 solution and day13 template

--- Code/12_aoc.py
+++ Code/12_aoc.py
@@ -1,17 +1,51 @@
 import os
 os.system("cls")
-print("\n\nNEW INPUT\n")
 
-def arrayToInt(array): return [int(i) for i in array]
+def getPaths(graph, start, record, path):
+    path.append(start)
 
-def removeArray(array, remove): return [i for i in array if (i != remove)]
+    if (start == "end"):
+        record.append(path.copy())
+    else:
+        for i in graph[start]:
+            if (i.islower() and i in path): continue
+            getPaths(graph, i, record, path.copy())
+    
+    return record
 
-def inc2dArray(array, x = 1): return [[j + x for j in i] for i in array]
+def getPaths2(graph, start, record, path):
+    path.append(start)
+    flag = False
 
-fp = open("../Input/12_input.txt", "r").read().split("\n")
+    for i in path:
+        if (i.islower() and path.count(i) > 1): 
+            flag = True
+            break
 
-inputNum = []
-for i in fp: 
-    inputNum.append(i)
+    if (start == "end"):
+        record.append(path.copy())
+    else:
+        for i in graph[start]:
+            if (i == "start" or (i.islower() and flag and i in path)): continue
+            getPaths2(graph, i, record, path.copy())
+    
+    return record
 
-print(inputNum)
+fp = open("../Input/12_input2.txt", "r").read().split("\n")
+
+inputNum = [i.split("-") for i in fp]
+
+path = {}
+for i,j in inputNum:
+    if (i not in path):
+        path[i] = [j]
+    else:
+        path[i].append(j)
+
+    if (j not in path):
+        path[j] = [i]
+    else:
+        path[j].append(i)
+
+print(len(getPaths(path, "start", [], [])))
+print(len(getPaths2(path, "start", [], [])))
