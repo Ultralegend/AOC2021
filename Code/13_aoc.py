e98a78e2f845ff6c5af1254e9bb31de2b12c505a
Ultralegend <johnphilipcarreon@gmail.com>
2021-12-14 05:50:14 +0800

    changed board from array to a set

--- Code/13_aoc.py
+++ Code/13_aoc.py
@@ -4,50 +4,37 @@ os.system("cls")
 
 def arrayToInt(array): return [int(i) for i in array]
 
-def plotBoard(board, height, width):
+def plotBoard(board):
+    width = max([i[0] for i in board]) + 1
+    height = max([i[1] for i in board]) + 1
+
     for j in range(height):
         for i in range(width):
-            if [i,j] not in board:
+            if (i,j) not in board:
                 print(" ", end="")
             else:
                 print("█", end="")
         print()
 
 def foldBoard(board, foldIns):
-    width = max([i[0] for i in board])
-    height = max([i[1] for i in board])
     dots = 0
 
     for fold, x in foldIns:
-        newBoard = []
+        newBoard = set()
         x = int(x)
 
-        if "y" in fold:
-            for i,j in board:
-                if j > x:
-                    newC = [i, abs(j - x * 2)]
-
-                    if newC not in newBoard: newBoard.append(newC)
-                elif [i,j] not in newBoard: newBoard.append([i,j])
-
-            height //= height // x
-
-        else:
-            for i,j in board:
-                if i > x:
-                    newC = [abs(i - x * 2), j]
-
-                    if newC not in newBoard: newBoard.append(newC)
-                elif [i,j] not in newBoard: newBoard.append([i,j])
-            
-            width //= width // x
+        for i, j in board:
+            if "y" in fold:
+                j = x - abs((j - x))
+            elif "x" in fold:
+                i = x - abs((i - x))
+            newBoard.add((i,j))
         
         if dots == 0: dots = len(newBoard)
         
         board = newBoard.copy()
     
     print(dots)
-    plotBoard(board, height, width)
     return board
 
 fp = open("../Input/13_input.txt", "r").read().split("\n\n")
@@ -56,4 +43,5 @@ inputNum = [arrayToInt(i.split(",")) for i in fp[0].split("\n")]
 
 foldNum = [i.split("=") for i in fp[1].split("\n")]
 
-foldBoard(inputNum, foldNum)
+board = foldBoard(inputNum, foldNum)
+plotBoard(board)
