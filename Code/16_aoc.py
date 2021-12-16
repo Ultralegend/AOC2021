2cc39ee5123a09e9d7232548db5e778fc16f946e
Ultralegend <johnphilipcarreon@gmail.com>
2021-12-17 07:59:26 +0800

    commit correct file

--- Code/16_aoc.py
+++ Code/16_aoc.py
@@ -1,96 +1,40 @@
 import os
-import math
+from math import prod
 
 os.system("cls")
 
-def getLiteralValue(bits, current):
-    ans = ""
-    idx = 0
+def hextobin(hex):
+    htob = {
+        "0": "0000", "1": "0001", "2": "0010", "3": "0011",
+        "4": "0100", "5": "0101", "6": "0110", "7": "0111",
+        "8": "1000", "9": "1001", "A": "1010", "B": "1011",
+        "C": "1100", "D": "1101", "E": "1110", "F": "1111"
+    }
+    result = ""
+
+    for i in hex: result += htob[i]
+    
+    return result
+
+def getLiteralValue(bits):
+    packetVer = int(bits[0:3], 2)
+    currentIdx = 6
+    literal = ""
+    
     while True:
-        temp = bits[idx:idx+5]
+        temp = bits[currentIdx:currentIdx + 5]
         
-        if temp[0] == "1":
-            ans += temp[1:]
-        elif temp[0] == "0":
-            ans += temp[1:]
-            idx += 5
-            break
-        
-        idx += 5
+        literal += temp[1:]
+        currentIdx += 5
+
+        if temp[0] == "0": break
     
-    return [current + idx, int(ans, 2)]
+    return [currentIdx, packetVer, int(literal, 2)]
 
-def evalOperatorInner(bits, current, typeId):
-    results = []
-    packetVer = 0
-    lengthId = 15 if bits[0] == "0" else 11
-    idx = 1 + lengthId
-
-    if lengthId == 15:
-        subLen = int(bits[1:1 + lengthId], 2) + idx
-
-        while idx < subLen:
-            packetVer += int(bits[idx:idx + 3], 2)
-            idx += 3
-            tI = int(bits[idx:idx + 3], 2)
-            idx += 3
-
-            if tI == 4:
-                retVal = getLiteralValue(bits[idx:], idx)
-                idx = retVal[0]
-                results.append(retVal[1])
-            else: 
-                retVal = evalOperatorInner(bits[idx:], idx, tI)
-                idx = retVal[0]
-                packetVer += retVal[1]
-                results.append(retVal[2])
-    
-    else:
-        subLen = int(bits[1:1+lengthId], 2)
-
-        for _ in range(subLen):
-            packetVer += int(bits[idx:idx + 3], 2)
-            idx += 3
-            tI = int(bits[idx:idx + 3], 2)
-            idx += 3
-
-            if tI == 4:
-                retVal = getLiteralValue(bits[idx:], idx)
-                idx = retVal[0]
-                results.append(retVal[1])
-            else: 
-                retVal = evalOperatorInner(bits[idx:], idx, tI)
-                idx = retVal[0]
-                packetVer += retVal[1]
-                results.append(retVal[2])
-    
-    if (len(results) > 1):
-        if typeId == 0:
-            results = sum(results)
-        elif typeId == 1:
-            results = math.prod(results)
-        elif typeId == 2:
-            results = min(results)
-        elif typeId == 3:
-            results = max(results)
-        elif typeId == 5:
-            if results[0] > results[1]: results = 1
-            else: results = 0
-        elif typeId == 6:
-            if results[0] < results[1]: results = 1
-            else: results = 0
-        elif typeId == 7:
-            if results[0] == results[1]: results = 1
-            else: results = 0
-    else:
-        results = results[0]
-
-    return [current + idx, packetVer, results]
-
-def evalOperatorPacket(bits):
-    results = []
+def evalOperatorPacket(bits, currentIdx):
     packetVer = int(bits[0:3], 2)
     typeId = int(bits[3:6] , 2)
+    operands = []
 
     lengthId = 15 if bits[6] == "0" else 11
     currentIdx = 7 + lengthId
@@ -99,67 +43,45 @@ def evalOperatorPacket(bits):
         subLen = int(bits[7:7 + lengthId], 2) + currentIdx
 
         while currentIdx < subLen:
-            packetVer += int(bits[currentIdx:currentIdx + 3], 2)
-            currentIdx += 3
-            tI = int(bits[currentIdx:currentIdx + 3], 2)
-            currentIdx += 3
+            tI = int(bits[currentIdx + 3:currentIdx + 6], 2)
 
             if tI == 4:
-                retVal = getLiteralValue(bits[currentIdx:], currentIdx)
-                currentIdx = retVal[0]
-                results.append(retVal[1])
-                
+                retVal = getLiteralValue(bits[currentIdx:])
             else: 
-                retVal = evalOperatorInner(bits[currentIdx:], currentIdx, tI)
-                currentIdx = retVal[0] 
-                packetVer += retVal[1]
-                results.append(retVal[2])
+                retVal = evalOperatorPacket(bits[currentIdx:], 0)
+            
+            currentIdx += retVal[0]
+            packetVer += retVal[1]
+            operands.append(retVal[2])
     
     else:
-        subLen = int(bits[7:7+lengthId], 2)
+        subLen = int(bits[7:7 + lengthId], 2)
 
         for _ in range(subLen):
-            packetVer += int(bits[currentIdx:currentIdx + 3], 2)
-            currentIdx += 3
-            tI = int(bits[currentIdx:currentIdx + 3], 2)
-            currentIdx += 3
+            tI = int(bits[currentIdx + 3:currentIdx + 6], 2)
 
             if tI == 4:
-                retVal = getLiteralValue(bits[currentIdx:], currentIdx)
-                currentIdx = retVal[0]
-                results.append(retVal[1])
+                retVal = getLiteralValue(bits[currentIdx:])
             else: 
-                retVal = evalOperatorInner(bits[currentIdx:], currentIdx, tI)
-                currentIdx = retVal[0] 
-                packetVer += retVal[1]
-                results.append(retVal[2])
-    
-    print(packetVer)
+                retVal = evalOperatorPacket(bits[currentIdx:], 0)
+            
+            currentIdx += retVal[0]
+            packetVer += retVal[1]
+            operands.append(retVal[2])
 
-    if (len(results) > 1):
-        if typeId == 0:
-            print(sum(results))
-        elif typeId == 1:
-            print(math.prod(results))
-        elif typeId == 2:
-            print(min(results))
-        elif typeId == 3:
-            print(max(results))
-        elif typeId == 5:
-            if results[0] > results[1]: print(1)
-            else: print(0)
-        elif typeId == 6:
-            if results[0] < results[1]: print(1)
-            else: print(0)
-        elif typeId == 7:
-            if results[0] == results[1]: print(1)
-            else: print(0)
-    else:
-        print(results[0])
+    result = {
+        0: sum,
+        1: prod,
+        2: min,
+        3: max,
+        5: lambda v: int(v[0] > v[1]),
+        6: lambda v: int(v[0] < v[1]),
+        7: lambda v: int(v[0] == v[1])
+    }
 
-fp = open("../Input/16_input2.txt", "r").read().strip()
+    return [currentIdx, packetVer, result[typeId](operands)]
 
-inputNum = bin(int(fp, 16))[2:]
-inputNum = "0" * (len(fp) * 4 - len(inputNum)) + inputNum
+fp = open("../Input/16_input.txt", "r").read().strip()
 
-evalOperatorPacket(inputNum)
+result = evalOperatorPacket(hextobin(fp), 0)
+print(result[1], result[2])
